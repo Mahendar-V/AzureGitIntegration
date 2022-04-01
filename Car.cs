ddfce2f2bafa5748e018e9d3beceef41b121fd77
Mahendar Vanaparthi <24625853+Mahendar-V@users.noreply.github.com>
2022-04-01 14:33:42 +0530

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AzureGitIntegration
+{
+    public partial class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ModelNumber { get; set; }
+        public string ManufactureYear { get; set; }
+        public string Manufacturer { get; set; }
+        public double? Rating { get; set; }
+    }
+}
---
+++ CarsDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace AzureGitIntegration
+{
+    public partial class CarsDbContext : DbContext
+    {
+        public CarsDbContext()
+        {
+        }
+
+        public CarsDbContext(DbContextOptions<CarsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.ManufactureYear)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manufacturer)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModelNumber)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
